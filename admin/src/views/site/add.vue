c2e4f167e28766082b2b30a49ef14354aafe881b
ErwinBothaDI <Erwin@devinspire.co.za>
2021-02-16 14:18:57 +0200

    Update add.vue

--- admin/src/views/site/add.vue
+++ admin/src/views/site/add.vue
@@ -5,11 +5,6 @@
           label="Site Name"
           v-model="crudManager.formObj.siteName"
         ></v-text-field>
-
-        <v-text-field
-          label="customerID"
-          v-model="crudManager.formObj.customerID"
-        ></v-text-field>
         <v-btn type="submit" color="success">Submit</v-btn>
       </v-form>
   </v-container>
