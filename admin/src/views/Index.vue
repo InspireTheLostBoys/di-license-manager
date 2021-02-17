acb742cfb4ede79c0ca7ca7caa37d1d8575439f0
lana3737 <lana@devinspire.co.za>
2021-02-17 13:01:43 +0200

    Update Index.vue

--- admin/src/views/Index.vue
+++ admin/src/views/Index.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>INDEX PAGE</h1>
+    <v-container>
+        <h1>INDEX PAGE</h1>
+        <v-btn @click="goTo" color="success"> Add license</v-btn>
+    </v-container>
 </template>
+
+<script>
+export default {
+    data: () => ({
+
+    }),
+    methods: {
+        goTo(){
+            let self = this;
+            self.$router.push('/new-or-existing-customer');
+        }
+    }
+}
+</script>
