efa3e29a02cf75ee2321d27a58288630b33626fa
lana3737 <lana@devinspire.co.za>
2021-02-19 14:15:55 +0200

    slot for dataTable

---
+++ admin/src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+      <v-data-table
+        :search="search"
+        :items="crudManager.list"
+        :headers="headers"
+      >
+      </v-data-table>
+    
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    search: String,
+    crudManager: Object,
+    headers: Array,
+  },
+
+  data: () => ({}),
+};
+</script>
--- admin/src/views/customer/list.vue
+++ admin/src/views/customer/list.vue
@@ -1,22 +1,13 @@
 <template>
   <v-container>
-    <ListWrapper header="Customer">
-      <template v-slot:search="params">
-        <v-data-table
-          :search="params.search"
-          :headers="headers"
-          :items="crudManager.list"
-        >
-          <template v-slot:item.actions="{ item }">
-            <v-btn disabled text @click="editItem(item)">edit</v-btn>
-            <v-btn disabled text @click="deleteItem(item)">delete</v-btn>
-          </template>
-        </v-data-table>
+    <ListWrapper header="Customer" >
+      <template v-slot:search="params">      
+      <List :search="params.search" :headers="headers" :crudManager="crudManager">
+      </List>
       </template>
     </ListWrapper>
 
     <v-btn disabled @click="crudManager.onAdd()" class="mr-3">Add</v-btn>
-
     <v-btn class="primary" @click="$router.push('/')">Back</v-btn>
   </v-container>
 </template>
@@ -24,9 +15,12 @@
 <script>
 import { CustomerCrud } from "../../libs/customer";
 import ListWrapper from "../../components/ListWrapper.vue";
+import List from "../../components/List.vue";
+
 export default {
   components: {
     ListWrapper,
+    List,
   },
   data() {
     return {
@@ -67,20 +61,6 @@ export default {
         },
       ],
     };
-  },
-  created() {
-    console.log(this.crudManager);
-  },
-  methods: {
-    clicked() {
-      alert("clicked from parent in child");
-    },
-    //     editItem(item) {
-    //         this.crudManager.onEdit(item);
-    //     },
-    //     deleteItem(item) {
-    //         this.crudManager.delete(item);
-    //     }
   },
 };
 </script>
